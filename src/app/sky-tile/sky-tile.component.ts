6f39094eb96fa464231241a454d5b2d7c537dec5
Sean McIntyre <seanmcintyre@gmail.com>
2018-05-01 16:20:02 -0400

    add gradients

--- src/app/sky-tile/sky-tile.component.ts
+++ src/app/sky-tile/sky-tile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { gradients } from './gradients';
+
 @Component({
   selector: 'sky-tile',
   templateUrl: './sky-tile.component.html',
@@ -17,4 +19,13 @@ export class SkyTileComponent implements OnInit {
     this.hour = this.hour + 1 <= 24 ? this.hour + 1 : 0;
   }
 
+  skyGradient() {
+    return gradient(this.hour);
+  }
+
 }
+
+const leadingZero = (i) => (i < 10 ? i = `0${i}` : i);
+const gradient = (hour) => ({
+  background: `linear-gradient(${gradients[leadingZero(hour)]})`
+});
